c7df35743e304fed49422390172dd018d6e9fba8
Alexandre Felipe Moreira Iargan Buch <felipe.m.buch@gmail.com>
2022-01-19 17:23:08 -0300

    Funcionando

---
+++ src/navigators/MainStack.js
@@ -0,0 +1,23 @@
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+
+import Preload from '../screens/Preload';
+import StarterStack from '../navigators/StarterStack';
+
+//import AppTab from '../screens/AppTab';
+
+const MainStack = createStackNavigator(
+  {
+    Preload,
+    StarterStack,
+    // AppTab,
+  },
+  {
+    initialRouteName: 'Preload',
+    defaultNavigationOptions: {
+      headerShown: false,
+    },
+  },
+);
+
+export default createAppContainer(MainStack);
